b0b93057dbe04550635d2a2e9dc7c9a6e1aed78b
mike <miketetteh751@outlook.com>
2022-05-19 17:03:35 +0000

    service patterns added

--- kas_client/auth/views.py
+++ kas_client/auth/views.py
@@ -6,5 +6,5 @@ from django.views import View
 class HomeView(View):
     def get(self, request):
         if request.user.is_authenticated:
-            return HttpResponse('<p>Welcome to successfully logged in.</p>')
+            return HttpResponse('<p>Welcome to successfully logged in.<br><a href="http://localhost:8001/accounts/logout">Logout</a></p>')
         return HttpResponseRedirect(reverse('auth:cas_ng_login'))
